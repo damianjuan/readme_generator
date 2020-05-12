3f5d93508b9670c6620a71ad05399b62aae282ba
Juan in a million <59286923+damianjuan@users.noreply.github.com>
2020-05-12 16:22:23 -0500

    markup for readme set up

--- Unsolved/utils/generateMarkdown.js
+++ Unsolved/utils/generateMarkdown.js
@@ -1,6 +1,29 @@
 function generateMarkdown(data) {
   return `
 # ${data.title}
+${data.description}
+
+##Table of Contents
+${data.table}
+
+### Installing
+${data.install}
+
+### Usage
+${data.usage}
+
+### Testing
+${data.test}
+
+
+### License 
+${data.license}
+
+### Contributors
+${data.contributors}
+
+### Questions
+${data.questions}
 
 `;
 }
