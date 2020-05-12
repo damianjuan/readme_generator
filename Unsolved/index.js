39d148ed9fd3618d2ec83a10c26553f690805733
Juan in a million <59286923+damianjuan@users.noreply.github.com>
2020-05-12 15:47:32 -0500

    added questions for user

--- Unsolved/index.js
+++ Unsolved/index.js
@@ -1,4 +1,55 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+
+const writeFileAsync = util.promisify(fs.writeFile);
+
 const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is the title of your project?"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Enter a description of the project."
+    },
+    {
+        type: "input",
+        name: "table",
+        message: "List the items for the table of contents."
+    },
+    {
+        type: "input",
+        name: "install",
+        message: "Enter the install instructions."
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "what is the usage?"
+    },
+    {
+        type: "input",
+        name: "licence",
+        message: "What is the licence?"
+    },
+    {
+        type: "input",
+        name: "contributors",
+        message: "List the contributors."
+    },
+    {
+        type: "input",
+        name: "test",
+        message: "What are the tests?"
+    },
+    {
+        type: "input",
+        name: "questions",
+        message: "Questions?"
+    },
 
 ];
 
