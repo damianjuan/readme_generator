ec315489761b01f9046be45985b5ad98757f7ae4
Juan in a million <59286923+damianjuan@users.noreply.github.com>
2020-05-15 18:31:49 -0500

    added require generateMarkdown.js to index.js

--- Unsolved/index.js
+++ Unsolved/index.js
@@ -1,6 +1,7 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const util = require("util");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 const writeFileAsync = util.promisify(fs.writeFile);
 
