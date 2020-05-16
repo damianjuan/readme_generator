4bb6ccce1d93685cbcb43d6880492a28f528b4e7
Juan in a million <59286923+damianjuan@users.noreply.github.com>
2020-05-16 16:45:36 -0500

    added badges choice in js and to markdown

--- Unsolved/utils/generateMarkdown.js
+++ Unsolved/utils/generateMarkdown.js
@@ -1,3 +1,23 @@
+generateBadge = () => {
+  var badge;
+  switch (data.status) {
+    case "Complete":
+      return badge = `https://img.shields.io/badge/<Build>-<Complete>-<green>`
+      break;
+    case "In Progress":
+      return badge = `https://img.shields.io/badge/<Build>-<In Progress>-<yellow>`
+      break;
+    case "Not Working":
+      return badge = `https://img.shields.io/badge/<Build>-<Not Working>-<red>`
+      break;
+
+    default:
+      break;
+  }
+
+}
+
+
 function generateMarkdown(data) {
   return `
 # ${data.title}
